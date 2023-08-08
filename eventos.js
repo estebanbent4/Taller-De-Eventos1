72346a16c132ed6bad4aaabeff419892df5e8a15
Esteban Bentancor <ebentancor341@gmail.com>
2023-08-07 23:04:09 -0300

    Commit 4 -branch 1

--- eventos.js
+++ eventos.js
@@ -1,15 +1,19 @@
-function myFuncion() {
-
+function myFuncion(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
     alert('Hola!');
 }
 
-
-
+function Div(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    alert('Hola! Soy el div.');
+}
 
 let elementoDiv = document.getElementById('div1');
 
-elementoDiv.addEventListener('click', function () {
-    alert('Hola! Soy el div.');
-});
+elementoDiv.addEventListener('click', Div);
 
+let boton1 = document.getElementById('boton');
 
+boton1.addEventListener('click',myFuncion);
